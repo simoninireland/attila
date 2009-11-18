5c2ddca775babcafd6d93ecb797e80ee5f217679
Simon Dobson <simon.dobson@computer.org>
2009-11-18 16:37:29 +0000

    Fixed some definitions

--- vm.fs
+++ vm.fs
@@ -1,4 +1,4 @@
-\ $Id: vm.fs,v 1.5 2007/06/13 15:57:39 sd Exp $
+\ $Id$
 
 \ This file is part of Attila, a minimal threaded interpretive language
 \ Copyright (c) 2007, UCD Dublin. All rights reserved.
@@ -22,35 +22,39 @@
 \ It's vitally important these match the definitions in the underlying
 \ C code of the virtual machine -- so important, in fact, that this file
 \ should really be derived automatically...
+\
+\ There are some overlaps that need to be sorted out, for example the inclusion
+\ of user variables that are irrelevant in embedded systems with no interaction.
 
 \ Status masks
-: IMMEDIATE-MASK    1 ;
-: REDIRECTABLE-MASK 2 ;
+1 CONSTANT IMMEDIATE-MASK
+2 CONSTANT REDIRECTABLE-MASK
 
 \ States
-: INTERPRETATION-STATE 0 ;
-: COMPILATION-STATE 1 ;
+0 CONSTANT INTERPRETATION-STATE
+1 CONSTANT COMPILATION-STATE
 
 \ User variables
-0 USER EXECUTIVE             \ xt of outer executive (OUTER by default)
-1 USER STATE                 \ interpreting or compiling
-2 USER BASE                  \ number base
-3 USER INPUTSOURCE           \ pointer to internal structure for input source
-4 USER (CURRENT-VOCABULARY)  \ current vocabulary
-5 USER TIB                   \ address of the TIB
-6 USER >IN                   \ input offset
+0 USER COLDSTART             \ xt of the word that cold-starts the system
+1 USER EXECUTIVE             \ xt of outer executive (OUTER by default)
+2 USER STATE                 \ interpreting or compiling
+3 USER BASE                  \ number base
+4 USER INPUTSOURCE           \ pointer to internal structure for input source
+5 USER >IN                   \ input offset
+6 USER TIB                   \ address of the TIB
+7 USER LAST                  \ xt of the most-recently-defined word
+8 USER TRACE                 \ trace (debugging) flag
 
 \ ---------- Truth values ----------
 
-1 CONSTANT TRUE
-0 CONSTANT FALSE
+0            CONSTANT FALSE
+FALSE INVERT CONSTANT TRUE
 
 
 \ ---------- Standard number bases ----------
+\ Note: by convention the system is in decimal at start-up
 
 : BINARY   2 BASE ! ;
 : DECIMAL 10 BASE ! ;
 : HEX     16 BASE ! ;
-
-DECIMAL
 
