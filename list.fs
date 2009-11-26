b26ef5d952d04ed547be2e4e377167ece789c5ed
Simon Dobson <simon.dobson@computer.org>
2009-11-26 13:28:03 +0000

    Made lists kind of polymorphic

--- list.fs
+++ list.fs
@@ -3,13 +3,15 @@
 \ Doubly-linked lists in data memory. These are typically used to allocate
 \ lists within the bodies of words. They should really be re-factored to
 \ allow the use of dynamic memory.
+\
+\ The element value is of arbitrary size, and nothing forces all elements
+\ to be the same, as long as you can avoid overruns.
 
-\ Create a new list element
-: LIST-ELEMENT \ ( n -- elem )
-    HERE SWAP
+\ Create the control part of new list element
+: (LIST-ELEMENT) \ ( -- elem )
+    HERE
     0 ,         \ back pointer
-    ,           \ content
-    0 , ;       \ forward pointer
+    0 , ;        \ forward pointer
 
 \ Navigate an element
 : <ELEMENT \ ( elem -- prev )
@@ -17,13 +19,25 @@
 : <ELEMENT! \ ( nprev elem -- )
     ! ;
 : ELEMENT> \ ( elem -- next )
-    2 CELLS + @ ;
-: ELEMENT>! \ ( nnext elem -- )
-    2 CELLS + ! ;
-: ELEMENT@ \ ( elem -- n )
     1 CELLS + @ ;
-: ELEMENT! \ ( n elem -- )
+: ELEMENT>! \ ( nnext elem -- )
     1 CELLS + ! ;
+: ELEMENT-VALUE \ ( elem -- vaddr )
+    2 CELLS + ;
+
+\ Lists of cells
+: CELL-ELEMENT \ ( n -- elem )
+    (LIST-ELEMENT) SWAP , ;
+: CELL-ELEMENT@ \ ( elem -- n )
+    ELEMENT-VALUE @ ;
+: CELL-ELEMENT! \ ( n elem -- )
+    ELEMENT-VALUE ! ;
+
+\ Lists of (constant) strings
+: STRING-ELEMENT \ ( addr n -- elem )
+    (LIST-ELEMENT) ROT S, ;
+: STRING-ELEMENT@ \ ( elem -- addr n )
+    ELEMENT-VALUE COUNT ;
 
 \ Traverse a list to the first or last element
 : <<ELEMENT \ ( elem -- first )
@@ -76,42 +90,45 @@
 \ sd: Note that these words work on list elements, *not*
 \ complete lists: if you pass an element halfway along a list
 \ to MAP it will apply only from there to the end
+\ sd: Note further that it's your problem to make these
+\ works work polymorphically: the words applied need to know
+\ what sort of value they're expecting
 
 \ Map a word over all the elements of a list. The word applied
-\ is expected to have a stack effect ( nbefore -- nafter ) and will
-\ be passed each element value 
+\ is expected to have a stack effect ( vaddr -- ) and will
+\ be passed the address of each element's value in turn
 : MAP \ ( xt elem -- )
     SWAP >R
     BEGIN
 	?DUP 0<>
     WHILE
-	    DUP DUP ELEMENT@ R@ EXECUTE SWAP ELEMENT!
+	    DUP ELEMENT-VALUE R@ EXECUTE
 	    ELEMENT>
     REPEAT
     RDROP ;
 
 \ Fold a word across the elements of a list "from the right". The word folded is
-\ expected to have stack effect ( accbefore n -- accafter ) and is passed
-\ an accululator and an element of the list from right to left
+\ expected to have stack effect ( accbefore vaddr -- accafter ) and is passed
+\ an accululator and an element's value address up the list from right to left
 : FOLDR \ ( xt acc elem -- facc )
     -ROT >R
     BEGIN
 	?DUP 0<>
     WHILE
-	    TUCK ELEMENT@ R@ EXECUTE
+	    TUCK ELEMENT-VALUE R@ EXECUTE
 	    SWAP <ELEMENT
     REPEAT
     RDROP ;
 
 \ Fold a word across the elements of a list "from the left". The word folded is
-\ expected to have stack effect ( accbefore n -- accafter ) and is passed
-\ an accululator and an element of the list from left to right
+\ expected to have stack effect ( accbefore vaddr -- accafter ) and is passed
+\ an accululator and an element's value address down the list from left to right
 : FOLDL \ ( xt acc elem -- facc )
     -ROT >R
     BEGIN
 	?DUP 0<>
     WHILE
-	    TUCK ELEMENT@ R@ EXECUTE
+	    TUCK ELEMENT-VALUE R@ EXECUTE
 	    SWAP ELEMENT>
     REPEAT
     RDROP ;
