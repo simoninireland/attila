7135423f032afa3ddc2222cd481e41a157ae1d59
Simon Dobson <simon.dobson@computer.org>
2009-12-10 16:29:46 +0000

    Refactored

--- colon.fs
+++ colon.fs
@@ -31,33 +31,9 @@
 
 \ ---------- Compilation helpers ----------
 
-\ Hooks run before the creation of a word and at its fianlisation
-\ sd: TBD
-
-\ Return the xt of the last defined word
-: LASTXT LAST USER @ ;
-
-\ Set a status mask for a colon definition
-: SET-STATUS \ ( mask xt -- )
-    >STATUS DUP C@
-    -ROT OR
-    SWAP C! ;
-
-\ Make the previous word IMMEDIATE
-: IMMEDIATE \ ( -- )
-    IMMEDIATE_MASK LASTXT SET-STATUS ;
-
-\ Make the previous word REDIRECTABLE
-: REDIRECTABLE \ ( -- )
-    REDIRECTABLE_MASK LASTXT SET-STATUS ;
-
 \ Test whether we're interpreting
 : INTERPRETING? \ ( -- f )
     STATE @ INTERPRETING = ;
-
-\ Test whether a word is IMMEDIATE
-: IMMEDIATE? \ ( xt -- f )
-    >STATUS C@ IMMEDIATE_MASK & ;
 
 \ Find the xt of the next word in the input source, or abort
 : ' \ ( "word" -- xt )
@@ -74,13 +50,13 @@
 
 \ Compile the top of the stack as a literal
 : LITERAL \ ( n -- )
-    ['] (LITERAL) COMPILE,
-    COMPILE, ; IMMEDIATE
+    ['] (LITERAL) XTCOMPILE,
+    XTCOMPILE, ; IMMEDIATE
 
 \ Compile the string on the tp of the stack as a literal
 : SLITERAL \ ( addr len -- )
-    ['] (SLITERAL) COMPILE,
-    S, ; IMMEDIATE
+    ['] (SLITERAL) XTCOMPILE,
+    SCOMPILE, ; IMMEDIATE
 
 \ Compile a "-delimited string from the input source as a literal
 : S" \ ( "string" -- )
@@ -94,16 +70,16 @@
 
 \ Enter interpretation state
 : [ \ ( -- )
-    INTERPRETING STATE ! ;
+    INTERPRETATION-STATE STATE ! ;
 
 \ Enter compilation state
 : ] \ ( -- )
-    COMPILING STATE ! ;
+    COMPILATION-STATE STATE ! ;
 
 \ The colon-definer
 : : \ ( "name" -- )
     START-DEFINITION
-    PARSE-WORD [ ' (:) >CFA @ ] (HEADER,)
+    PARSE-WORD [ ' (:) CFA@ ] (HEADER,)
     ] ;
 
 \ Complete a colon-definition
@@ -139,37 +115,3 @@
     EXHAUSTED? UNTIL ;
 
 
-\ ---------- Including files ----------
-
-\ Load a file
-: (LOAD) \ ( fh -- )
-    INPUTSOURCE DUP @
-    2 PICK 2 PICK !
-
-    \ run the executive on its original stack
-    >R >R >R
-    EXECUTIVE @ EXECUTE
-    R> R> R>
-
-    -ROT FILE-CLOSE SWAP ! ;
-
-\ Open a file using the given word as file-finder
-: (OPEN-FILE) \ ( addr len opener -- fh )
-    >R 2DUP R> EXECUTE
-    DUP IF
-	ROT 2DROP
-    ELSE
-	DROP TYPE SPACE S" not accessible" ABORT
-    THEN ;
-
-\ Load a file
-: LOAD \ ( addr len -- )
-    ['] FILE-OPEN (OPEN-FILE) (LOAD) ;
-
-\ Include a file, making use of the include path
-: INCLUDE \ ( addr len -- )
-    ['] FILE-OPEN-INCLUDE (OPEN-FILE) (LOAD) ;
-
-\ Load the next file from the input stream
-: #INCLUDE \ ( "filename" -- )
-    PARSE-WORD INCLUDE ; IMMEDIATE
