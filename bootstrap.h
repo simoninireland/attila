72f827699dafbd4cbf3d4d212709e2fdbc5f5ef4
Simon Dobson <simon.dobson@computer.org>
2010-01-28 10:34:41 +0000

    Moved stack routines inline

--- bootstrap.h
+++ bootstrap.h
@@ -29,11 +29,6 @@
 #include <string.h>
 #include <setjmp.h>
 
-#define WHITESPACE " \t\n"
-
-// The available numeric digits
-static const char *digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
 
 // ---------- Debugging ----------
 
@@ -42,7 +37,19 @@ static const char *digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 // ---------- Stacks ----------
 
-#include "x-stack-gcc.h"
+// Data stack macros
+#define PUSH_CELL( cell ) (*(data_stack++)) = ((CELL) cell)
+#define POP_CELL() (CELL) ((--data_stack < data_stack_base) ? DIE("data stack underflow") : (*data_stack))
+#define DATA_STACK_ITEM( n ) (CELLPTR) (data_stack - (n) - 1) 
+#define DATA_STACK_DEPTH() (data_stack - data_stack_base)
+#define DATA_STACK_RESET() data_stack = data_stack_base
+
+// Return stack macros
+#define PUSH_RETURN( xt ) (*(return_stack++)) = ((XT) xt)
+#define POP_RETURN() (XT) ((--return_stack < return_stack_base) ? DIE("return stack underflow") : (*return_stack))
+#define PEEK_RETURN() (XT) ((return_stack == return_stack_base) ? DIE("peeking empty return stack") : (*(return_stack - 1)))
+#define RETURN_STACK_ITEM( n ) (XTPTR) (return_stack - n - 1) 
+#define RETURN_STACK_RESET() return_stack = return_stack_base
 
 
 // ---------- Compilation helper macros ----------
@@ -56,7 +63,7 @@ static const char *digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 #define USER_BASE 5
 #define USER_INPUTSOURCE 6
 #define USER_OUTPUTSINK 7
-#define USER_OFFSET 8
+#define USER__IN 8
 #define USER_TIB 9
 #define USER_LAST 10
 #define USER_TRACE 11
