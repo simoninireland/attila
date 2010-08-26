fd234331e5af56fbfb8de907d99c72a15468066d
Simon Dobson <simon.dobson@computer.org>
2010-08-26 10:39:43 +0000

    Initial implementation

---
+++ records.fs
@@ -0,0 +1,84 @@
+\ $Id$
+
+\ Records
+\
+\ Records are simply blocks of memory with accessor words returning the
+\ address for each field. Unlike other implemnetations we just provide
+\ address-based accessors, not type-based storage and retrieval words
+\ (although that might be an option for the future).
+\
+\ Record types are created by, for example:
+\
+\ RECORD: PERSON
+\    1 CELLS  FIELD: PERSON-IDENTIFIER
+\    50 CHARS FIELD: PERSON-NAME
+\ ;RECORD
+\
+\ Instances are created and accessed by, for example:
+\
+\ PERSON PERSON1
+\ 1234 PERSON1 PERSON-IDENTIFIER !
+\ S" Joe Bloggs" PERSON1 PERSON-NAME SMOVE
+\ PERSON1 PERSON-NAME COUNT TYPE
+\
+\ Record types can be extended, adding extra filds to an existing type, using:
+\
+\ RECORD: EMPLOYEE EXTENDS: PERSON
+\     1 CELLS FIELD: EMPLOYEE-NUMBER
+\ ;RECORD
+\
+\ where any instance of EMPLOYEE also has all the fields of PERSON.
+\
+\ All fields are aligned on cell boundaries.
+
+
+\ Return the size of a record type in bytes from its xt
+: RECORDTYPESIZE ( xt -- bs )
+    >BODY @ ;
+
+\ Return the size in bytes of the record
+: RECORDSIZE ( rec -- bs )
+    @ ;
+
+\ Create a record from a name and the xt of its type. This is functionally the behaviour
+\ performed by the record type word, but it can be called like this to create
+\ records for run-time-determined types
+: (CREATE-RECORD) ( rec addr n -- )
+    (DATA)                \ create the record
+    RECORDTYPESIZE DUP ,  \ record the size...
+    1 CELLS - ALLOT ;     \ ...and allocate the space needed
+
+\ Basic record type creator
+: (RECORD:) ( off addr n -- rectype off )
+    (CREATE)
+    TOP 0 ,           \ placeholder for size
+    SWAP 1 CELLS +    \ initial offset within record, including space for size field
+  DOES> ( "name" -- )
+    DATA              \ create the record
+    @ DUP ,           \ record the size...
+    1 CELLS - ALLOT ; \ ...and allocate the space needed
+
+\ Create a new record type with the given name
+: RECORD: ( "name" -- rectype off )
+    0 PARSE-WORD (RECORD:) ;
+
+\ Mark the record type as extending another, i.e. having at least the same fields
+: EXTENDS: ( off "base" -- off1 )
+    ' RECORDTYPESIZE 1 CELLS -    \ offset is size of base record type minus its size field
+    + ;
+
+\ Finalise the record type structure
+: ;RECORD ( rectype off -- )
+    SWAP ! ;
+
+\ Define a field with a size of bs bytes
+: FIELD: ( off bs "name" -- off1 )
+    CREATE
+    OVER ,
+    + ALIGN
+  DOES> ( rec f -- addr )
+    @ + ;
+
+\ Copy a record to given address
+: RECORDMOVE ( rec addr -- )
+    OVER RECORDSIZE CMOVE ;
