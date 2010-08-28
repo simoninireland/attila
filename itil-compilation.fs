aa9d138d0ad6afbdacc69266d17df729e331efa1
Simon Dobson <simon.dobson@computer.org>
2010-08-28 11:57:17 +0000

    Moved jump compilation words here

--- itil-compilation.fs
+++ itil-compilation.fs
@@ -2,5 +2,23 @@
 
 \ Words needed to support compilation, indirect-threaded version
 
+\ Compile the ct of an xt
 : CTCOMPILE, XTCOMPILE, ;
+
+\ Compile the end of a word
 : NEXT, 0 COMPILE, ;
+
+\ Prepare for a forward jump
+: JUMP-FORWARD ( -- a )
+    2 USERVAR ( TOP ) @
+    0 COMPILE, ;
+
+\ Resolve a jump from the address on the stack to here
+: JUMP-HERE ( a -- )
+    2 USERVAR ( TOP ) @ OVER -
+    SWAP ! ;
+
+\ Resolve a jump from here backwards to the address on the stack
+: JUMP-BACKWARD ( a -- )
+    2 USERVAR ( TOP ) @ -
+    COMPILE, ;
