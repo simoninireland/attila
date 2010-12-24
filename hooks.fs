5fc257cbb584beb5802bbd95001a95662a66db34
Simon Dobson <simon.dobson@computer.org>
2010-12-24 11:26:03 +0000

    Updated to new, simpler architecture

--- hooks.fs
+++ hooks.fs
@@ -22,69 +22,54 @@
 \
 \ Hooks collect together a list of words that can be changed dynamically.
 \ The idea is that a hook collects together the words that should happen
-\ a particular "strategic" point -- for exaple when a file is loaded,
+\ a particular "strategic" point -- for example when a file is loaded,
 \ or when a word is defined -- and lets different parts of the system
 \ specify some behaviour to be performed then.
 \
-\ Given the dynamic nature of a hook, hooked words shouldn't make
-\ any assumptions about the order of their execution.
+\ Each word hung on a hook should have stack comment ( ... -- ... f ),
+\ leaving a flag on the stack indicating whether the hook should keep
+\ running words. A flag of TRUE stops execution. The flag returned by the
+\ last word run is left on the stack after running the hook. This means
+\ that hooks can be used to "try" operations, with the hook succeeding
+\ as soon as a word returns TRUE. Hooked words are run oldest-to-youngest.
 \
-\ Hooks allocate a static amount of space for their hooked words, which
-\ may be a little wasteful but is efficient. A linked-list representation
-\ might save a little memory but would be more complicated to manage.
+\ Hooks are dynamic, so the structure is maintained in data memory.
 
-\ The number of words that can be hung from a hook -- modify with care
-10 VALUE MAX-HOOKED-WORDS
+\ Create a hook with the given name
+: (HOOK) ( addr n -- )
+    (DATA)
+    0 A, ;
 
-\ Create a named hook
-: (HOOK) \ ( addr n -- )
-    (CREATE)
-    MAX-HOOKED-WORDS DUP ,
-    0 DO
-	0 ,
-    LOOP
-    DOES>
-      1 CELLS + ;
-
-\ Create a hook from the next word
-: HOOK \ ( "name" -- )
+\ Create a hook
+: HOOK ( "name" -- )
     PARSE-WORD (HOOK) ;
-
-\ Add a word to a hook. The same word may appear more than once on a hook
-: ADD-TO-HOOK \ ( xt addr -- ) 
-    DUP 1 CELLS - @            \ xt addr n
-    0 DO
-	DUP @ 0= IF            \ xt addr
-	    !
-	    0 0 LEAVE
-	ELSE
-	    1 CELLS +
-	THEN
-    LOOP
-    DROP 0<> IF
-	S" Hook full" ABORT
-    THEN ;
-
-\ Remove a word from a hook. Removes only one instance, if there
-\ are several
-: REMOVE-FROM-HOOK \ ( xt addr -- )
-    DUP 1 CELLS - @            \ xt addr n
-    0 DO
-	2DUP @ = IF            \ xt addr
-	    0 SWAP !
-	    0 LEAVE
-	ELSE
-	    1 CELLS +
-	THEN
-    LOOP
-    2DROP ;
-
-\ Run a hook
-: RUN-HOOK \ ( addr -- )
-    DUP 1 CELLS - @ 0 DO
-	DUP @ ?DUP 0<> IF
-	    EXECUTE
-	THEN
-	1 CELLS +
-    LOOP
-    DROP ;
+    
+\ Traverse a hook to the final link
+: HOOK>LINK ( h -- addr )
+    BEGIN
+	DUP @ DUP 0<>
+    WHILE
+	    NIP
+    REPEAT
+    DROP ;  
+    
+\ Add a word to a hook
+: ADD-TO-HOOK ( xt h -- )
+    HOOK>LINK
+    HERE SWAP A!  \ link previous link to HERE
+    0 A,          \ store link
+      A, ;        \ store xt of hooked word
+  
+\ Run the words hung on a hook
+: RUN-HOOK ( h -- f )
+    BEGIN
+	@ DUP 0<>
+    WHILE
+	    DUP >R
+	    1 CELLS + @ EXECUTE
+	    R> SWAP DUP 0<> IF
+		NIP EXIT
+	    ELSE
+		DROP
+	    THEN
+    REPEAT ;
