b64dd4e19af18fb04c1547639b75fd3602c83f10
Simon Dobson <simon.dobson@computer.org>
2010-12-24 15:40:19 +0000

    Refactored inclusion with paths

--- load.fs
+++ load.fs
@@ -20,8 +20,6 @@
 
 \ Source file inclusion
 
-\ ---------- Loading sourcefiles ----------
-
 \ Load a file
 : (LOAD) \ ( fh -- )
     (<FROM)
@@ -37,14 +35,3 @@
 	(LOAD)
     THEN ;
 
-    
-\ ---------- Including files ----------
-\ sd: no include path yet
-
-\ Include a file, making use of the include path
-: INCLUDED \ ( addr len -- )
-    LOAD ;
-
-\ Load the next file from the input stream
-: INCLUDE \ ( "filename" -- )
-    PARSE-WORD INCLUDED ;
