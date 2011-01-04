d61e8ecd2dea242377c748c4387c4e704ad57a35
Simon Dobson <simon.dobson@computer.org>
2011-01-04 21:15:35 +0000

    Upgraded to support include paths

--- include.fs
+++ include.fs
@@ -19,10 +19,90 @@
 \ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
 
 \ Source file inclusion, with include paths
-\ Old version
+\
+\ Inclusion searches for the named file appended to each path
+\ in turn until one is found.
 
+
+\ ---------- Paths ----------
+
+\ The base of the include paths chain
+DATA INCLUDE-PATH /CELL ALLOT
+
+\ The buffer for the full file path
+DATA INCLUDE-FILENAME 256 ALLOT
+
+\ Add a path to the include paths chain
+: ADD-INCLUDE-PATH ( addr n -- )
+    \ find latest link in chain
+    INCLUDE-PATH
+    BEGIN
+	DUP @ DUP 0<>
+    WHILE
+	    NIP
+    REPEAT
+    DROP
+
+    \ add the new path to the chain
+    HERE SWAP A!  \ link previous link to HERE
+    0 ,           \ store link
+      S, ;        \ store string
+
+\ Construct a file path from a chain component and a file stem
+\ sd: this includes some pretty heroic stack manipulations it'd be
+\ good to do without...
+: MAKE-INCLUDE-FILENAME ( addr n caddr -- addr' n' )
+    DUP C@ SWAP 1+ SWAP                  ( addr n paddr pn )
+    2DUP INCLUDE-FILENAME                ( addr n paddr pn paddr pn fn )
+    SWAP CMOVE                           ( addr n paddr pn )
+    NIP                                  ( addr n pn )
+    S" /"                 \ sd: should reflect system separator
+    2DUP                                 ( addr n pn saddr sn saddr sn )
+    4 PICK INCLUDE-FILENAME + SWAP CMOVE ( addr n pn saddr sn )
+    NIP +                                ( addr n tn )
+    ROT 2DUP                             ( tn addr n addr n )
+    4 PICK INCLUDE-FILENAME + SWAP CMOVE ( tn addr n )
+    NIP + INCLUDE-FILENAME SWAP ;
+
+\ Traverse the chain concatenating the given file name with
+\ the include path until we find one that exists, returning its
+\ full path
+: FIND-INCLUDE-FILE ( addr n -- addr' n' -1 | 0 )
+    INCLUDE-PATH
+    BEGIN
+	@ DUP 0<>
+    WHILE
+	    DUP /CELL + 2OVER -ROT MAKE-INCLUDE-FILENAME
+	    2DUP READABLE? IF
+		-ROT DROP 2SWAP 2DROP TRUE EXIT
+	    ELSE
+		2DROP
+	    THEN
+    REPEAT
+    DROP 2DROP FALSE ;
+ 
+
+\ ---------- Inclusion ----------
+
+
+\ Find the given file either absolutely or via the include path chain
+\ and load it if found
 : INCLUDED \ ( addr len -- )
-    LOAD ;
+    \ try as-is first
+    2DUP READABLE? IF
+	\ found it, load it
+	LOAD
+    ELSE
+	\ not available directly, try via inclusion
+	2DUP FIND-INCLUDE-FILE IF
+	    \ found it, load it
+	    2SWAP 2DROP
+	    LOAD
+	ELSE
+	    TYPE 32 EMIT ( SPACE ) S" not found in include path" ABORT
+	THEN
+    THEN ;
 
+\ Include the following file
 : INCLUDE \ ( "filename" -- )
     PARSE-WORD INCLUDED ;
