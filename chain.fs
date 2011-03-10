ba565020b0769e0580f746e17a488bf60e67eded
Simon Dobson <simon.dobson@computer.org>
2011-03-10 21:53:03 +0000

    Initial implementation

---
+++ chain.fs
@@ -0,0 +1,71 @@
+\ $Id$
+
+\ This file is part of Attila, a retargetable threaded interpreter
+\ Copyright (c) 2007--2011, Simon Dobson <simon.dobson@computer.org>.
+\ All rights reserved.
+\
+\ Attila is free software; you can redistribute it and/or
+\ modify it under the terms of the GNU General Public License
+\ as published by the Free Software Foundation; either version 2
+\ of the License, or (at your option) any later version.
+\
+\ Attila is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+\
+\ You should have received a copy of the GNU General Public License
+\ along with this program; if not, write to the Free Software
+\ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+
+\ A chain of values
+\
+\ These are used to store chains of things like strings in data memory.
+\ Each link in the chain consists of a data block and a pointer to the next
+\ link.
+\
+\ Given their nature the data in links can't be updated -- well not without
+\ extreme care not to overflow the data block size, anyway...
+\
+\ You can use these functions directly to store strings. At the moment they
+\ waste some space by using a cell instead of a byte for the length. This
+\ might change, and we might restrict the data block size to a byte's-worth.
+
+\ Return the next link in a chain, or zero
+: NEXT-LINK-IN-CHAIN ( link -- link | 0 )
+    @ ;
+
+\ Add a link to a chain. The data added to the link is given by the
+\ address and count pair
+: ADD-LINK-TO-CHAIN ( addr n link -- )
+    \ find latest link in chain
+    BEGIN
+	DUP NEXT-LINK-IN-CHAIN ?DUP 0<>
+    WHILE
+	    NIP
+    REPEAT
+
+    \ add the new link to the chain
+    ALIGNED HERE SWAP A!     \ link previous link to HERE
+    0 ,                      \ store blank link
+    DUP ,                    \ store the size
+    HERE OVER ALLOT          \ allot space for data block
+    SWAP CMOVE ;             \ store the data block
+
+\ Return the size and address of a link data block
+: DATA-LINK-IN-CHAIN ( link -- addr n )
+    /CELL +                   \ step over link
+    DUP @ SWAP /CELL + SWAP ; \ data block address and size
+
+\ Place the entire chain of data blocks on the stack, shallowest link first,
+\ topped by a count
+: ALL-DATA-LINKS-IN-CHAIN ( link -- addrj nj -- addr0 n0 j+1 )
+    0 SWAP
+    BEGIN
+	NEXT-LINK-IN-CHAIN DUP 0<>
+    WHILE ( c l )
+	    DUP DATA-LINK-IN-CHAIN ( c l addr n )
+	    2SWAP
+	    SWAP 1+ SWAP
+    REPEAT
+    DROP ;
