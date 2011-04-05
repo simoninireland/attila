4e5011700acc478378c36a7c37e3d23f2eeda7c8
Simon Dobson <simon.dobson@computer.org>
2011-04-05 15:52:23 +0000

    Added EVALUATE tests

--- test/core-system-tests.fs
+++ test/core-system-tests.fs
@@ -29,6 +29,7 @@ testcases test/words.fs
 testcases test/allot.fs
 testcases test/ascii.fs     \ assuming system uses an ASCII encoding
 testcases test/strings.fs
+testcases test/evaluate.fs
 testcases test/formatting.fs
 testcases test/loops.fs
 testcases test/continuations.fs
