07249455f2b6ef7f1db7f8c40781a522d8d28fe4
Simon Dobson <simon.dobson@computer.org>
2011-04-05 15:49:19 +0000

    Initial tests for EVALUATE

---
+++ test/evaluate.fs
@@ -0,0 +1,35 @@
+\ $Id$
+
+\ This file is part of Attila, a retargetable threaded interpreter
+\ Copyright (c) 2007--2010, Simon Dobson <simon.dobson@computer.org>.
+\ All rights reserved.
+\
+\ Attila is free software; you can redistribute it and/or
+\ modify it under the terms of the GNU General Public License
+\ as published by the Free Software Foundation; either version 2
+\ of the License, or (at your option) any later version.
+\
+\ Attila is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+\
+\ You should have received a copy of the GNU General Public License
+\ along with this program; if not, write to the Free Software
+\ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+
+\ Tests of evaluating strings
+
+TESTCASES" String evaluate as source code"
+
+\ sd: EVALUATE doesn't play well with INCLUDE at present, as it
+\ forces a refill. If we move to one-buffer-per-file we won't
+\ need the newlines
+
+{ S" 1 2 +" EVALUATE
+-> 3 }
+
+{ S" 1 TRUE [IF] 1 [ELSE] 2 [THEN] +" EVALUATE
+-> 2 }
+
+
