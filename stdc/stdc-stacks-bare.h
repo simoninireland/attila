3b1188f1ddba92b4fef63e75f254f575e76ec21f
Simon Dobson <simon.dobson@computer.org>
2011-04-06 17:26:03 +0000

    Bare stacks without bounds checks, for efficiency

--- stdc/stdc-stacks-bare.h
+++ stdc/stdc-stacks-bare.h
@@ -18,7 +18,7 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
 
-// "Bare" stacks without much in the way of safety checking
+// "Bare" stacks without safety checking
 
 #ifndef STDC_STACKS_BARE
 #define STDC_STACKS_BARE
@@ -29,14 +29,14 @@ XTPTR return_stack, return_stack_base;   // return stack
 
 // Data stack macros
 #define PUSH_CELL( cell ) (*(data_stack++)) = ((CELL) cell)
-#define POP_CELL() (CELL) ((--data_stack < data_stack_base) ? DIE("data stack underflow") : (*data_stack))
+#define POP_CELL() (CELL) (*(--data_stack))
 #define DATA_STACK_ITEM( n ) (CELLPTR) (data_stack - (n) - 1) 
 #define DATA_STACK_DEPTH() (data_stack - data_stack_base)
 #define DATA_STACK_RESET() data_stack = data_stack_base
 
 // Return stack macros
 #define PUSH_RETURN( xt ) (*(return_stack++)) = ((XT) xt)
-#define POP_RETURN() (XT) ((--return_stack < return_stack_base) ? DIE("return stack underflow") : (*return_stack))
+#define POP_RETURN() (XT) (*(--return_stack))
 #define RETURN_STACK_DEPTH() (return_stack - return_stack_base)
 #define RETURN_STACK_ITEM( n ) (XTPTR) (return_stack - (n) - 1)
 #define PEEK_RETURN() (XT) *(RETURN_STACK_ITEM(0))
