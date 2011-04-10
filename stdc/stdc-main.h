ede63ec3ad85a90ef31c28d005a984c9718303fb
Simon Dobson <simon.dobson@computer.org>
2011-04-10 09:38:48 +0000

    Added signal handling to make development VMs more robust

--- stdc/stdc-main.h
+++ stdc/stdc-main.h
@@ -18,13 +18,16 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
 
-// Basic interactive main() routine
+// Basic interactive main() routine. This makes use of standard library
+// functions like longjmps and signals to try to stay running in the
+// presence of memory and other errors common in developing Forth code. 
 
 #ifndef STDC_MAIN
 #define STDC_MAIN
 
 #include <stdlib.h>
 #include <setjmp.h>
+#include <signal.h>
 
 
 // Warm-start vector
@@ -37,10 +40,33 @@ extern CELLPTR user_variable( int );
 // Forth level start word
 extern VOID init( XT );
 
+
+// Signal handler warm-starts the interpreter by jumping to the
+// installed warm-srart longjmp with a handy message. Only the
+// common signals are caught: others do their default action
+void
+signal_handler( int sig ) {
+  switch(sig) {
+  case SIGINT:
+    DIE("Interrupt");
+
+  case SIGBUS:
+  case SIGSEGV:
+    DIE("Memory violation");
+  }
+}
+
+
+// Main routine
 int
 main( int argc, char *argv[] ) {
   XT _xt = NULL;
   CELL tmpstack[3];
+
+  // install signal handlers
+  signal(SIGINT, &signal_handler);
+  signal(SIGBUS, &signal_handler);
+  signal(SIGSEGV, &signal_handler);
 
   // set up a temporary stack, enough to call simple primitives
   // so we can d user variable calculations etc
