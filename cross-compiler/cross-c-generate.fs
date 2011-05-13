f35c781bc0af40a9744841a5f24b4b8e671c7848
Simon Dobson <simon.dobson@computer.org>
2011-05-13 17:59:18 +0000

    Added runtime name, changed image name to target name

--- cross-compiler/cross-c-generate.fs
+++ cross-compiler/cross-c-generate.fs
@@ -122,7 +122,9 @@
 : B>KB ( nb -- nkb )
     1024 /MOD SWAP IF 1+ THEN ;
 : SUMMARISE-IMAGE ( -- )
-    ." Image name: " [CROSS] VM-IMAGE-NAME TYPE SPACE ." (" CROSS-COMPILER-OUTPUT-FILE TYPE ." )" CR
+    ." Target name: "        [CROSS] VM-TARGET-NAME TYPE SPACE
+                             ." (" CROSS-COMPILER-OUTPUT-FILE TYPE ." )" CR
+    ." Runtime: "            [CROSS] VM-RUNTIME TYPE CR
     ." VM characteristics: " [CROSS] /CELL . SPACE ." bytes/cell, "
                              [CROSS] BIGENDIAN? IF ." big-endian" ELSE ." little-endian" THEN CR
     20 SPACES                [CROSS] /CHAR . SPACE ." bytes/char" CR
